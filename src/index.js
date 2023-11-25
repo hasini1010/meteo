cc6a982320e90b01777955146b48f2175f04a85c
Hasini Kanchetty <hasinikanchetty@gmail.com>
2023-11-25 16:12:05 +0530

    search functionality using api

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response)
+{
+let temperatureElement=document.querySelector("#temperature");
+let temperature=response.data.temperature.current;
+let cityElement=document.querySelector("#city");
+cityElement.innerHTML=response.data.city;
+temperatureElement.innerHTML=Math.round(temperature);
+}
+function searchCity(city)
+{
+    //make an api call and update the interface 
+    let apiKey="69aoab601t0ee004cd83d99857f5173e";
+    let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather)
+}
 function handleSearchSubmit(event)
 {
     event.preventDefault();
     let searchInput=document.querySelector("#search-form-input");
     let cityElement=document.querySelector("#city");
     cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement=document.querySelector("#search-form");
